8c5e6f0ac5d46edbebb1c47f5aff5192decf08ee
Thirumurugan <thiruks.dev@gmail.com>
2022-09-05 17:16:30 +0530

    All added

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send("Hello");
+})
+
+app.listen(port , ()=>{
+    console.log("App is running");
+})
